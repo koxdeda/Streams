a86bf6ca7a2585305bf6a7c1907298aa25b2fdd3
Evgenii Dedushkevich <kox.deda@yandex.ru>
2020-05-14 22:19:58 +0300

    Added json-server and some forms

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,15 @@
-import {SIGN_IN, SIGN_OUT} from './types';
+import streams from '../apis/streams';
+import history from '../history';
+
+import {
+        SIGN_IN,
+        SIGN_OUT,
+        CREATE_STREAM,
+        FETCH_STREAM,
+        FETCH_STREAMS,
+        DELETE_STREAM,
+        EDIT_STREAM
+    } from './types';
 
 export const signIn = (userId) => {
     return {
@@ -12,3 +23,39 @@ export const signOut = () => {
         type: SIGN_OUT
     };
 };
+
+export const createStream = formValues => async (dispatch, getState) => {
+    const { userId } = getState().auth;
+    const response = await streams.post('/streams', { ...formValues, userId });
+
+    dispatch({ type: CREATE_STREAM, payload: response.data })  ;
+
+    history.push('/');
+};
+
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams');
+
+    dispatch({ type: FETCH_STREAMS, payload: response.data }) ; 
+}
+
+export const fetchStream = (id) => async dispatch => {
+    const response = await streams.get(`/streams/${id}`);
+
+    dispatch({ type: FETCH_STREAM, payload: response.data })  ;
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+    const response = await streams.patch(`/streams/${id}`, formValues);
+
+    dispatch({ type: EDIT_STREAM, payload: response.data });
+
+    history.push('/');
+};
+
+export const deleteStream = (id, formValues) => async dispatch => {
+    await streams.deleet(`/streams/${id}`, formValues);
+
+    dispatch({ type: DELETE_STREAM, payload: id })  ;
+};
+
