4946112ca4ef12cc4fb1fc3f6faf05d2d067bd66
Evgenii Dedushkevich <kox.deda@yandex.ru>
2020-05-14 11:49:51 +0300

    Google Auth Complete

---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+
+import App from './components/App';
+import reducers from './reducers';
+
+
+const composeEnhacnecers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducers,
+    composeEnhacnecers(applyMiddleware())
+    );
+
+ReactDOM.render(
+    <Provider store = {store}>
+    <App />
+    </Provider>,
+     document.querySelector('#root')
+     );
